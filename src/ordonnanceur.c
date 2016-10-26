fbd3dfe054f22d52111afeabb6f4c3c06dcf540d
Maxime Deloche <maxime.deloche@ensimag.grenoble-inp.fr>
2016-10-26 14:29:04 +0200

    Fini premiere etape avec 2 processus qui se passent la main explicitement

--- src/ordonnanceur.c
+++ src/ordonnanceur.c
@@ -1,15 +1,40 @@
 #include <ordonnanceur.h>
+#include <string.h>
+#include <cpu.h>
+
+
+void init_idle(processus* tab_proc)
+{
+	tab_proc[0].pid = 0;
+	strcpy(tab_proc[0].nom, "idle");
+	tab_proc[0].etat_courant = ELU;
+}
+
+void init_proc1(processus* tab_proc)
+{
+	tab_proc[1].pid = 1;
+	strcpy(tab_proc[1].nom, "proc1");
+	tab_proc[1].etat_courant = ACTIVABLE;
+	tab_proc[1].sauv_reg[1] = (int32_t)&(tab_proc[1].pile[TAILLE_PILE-1]);
+	tab_proc[1].pile[TAILLE_PILE-1] = (int32_t)proc1;
+}
 
 void idle(void)
 {
-	printf("[idle]  : Je passe la main à proc1\n");
-	ctx_sw();
+	for (int i = 0; i < 3; i++) {
+		printf("[idle]  : Je passe la main à proc1\n");
+		ctx_sw(TAB_PROC[0].sauv_reg, TAB_PROC[1].sauv_reg);
+		printf("[idle]  : proc1 m'a rendu la main\n");
+	}
+	printf("[idle]  : Je bloque le systeme\n");
+	hlt();
 }
 
 
 void proc1(void)
 {
-	printf("[proc1] : idle m'a donné la main\n");
-	printf("[proc1] : J'arrete le système\n");
-	hlt();
+	for (;;) {
+		printf("[proc1] : idle m'a donné la main\n");
+		ctx_sw(TAB_PROC[1].sauv_reg, TAB_PROC[0].sauv_reg);
+	}
 }
--- src/ordonnanceur.h
+++ src/ordonnanceur.h
@@ -2,24 +2,34 @@
 #define ORDONNANCEUR_H
 
 #include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define TAILLE_PILE 512
 #define NB_PROC 2
 
-enum etat = {ELU, ACTIVABLE};
+enum etat {ELU, ACTIVABLE};
 
 typedef struct processus {
 	int32_t pid;
-	char[20] nom;
+	char nom[20];
 	enum etat etat_courant;
-	uint32_t[5] sauv_reg;
-	uint32_t[TAILLE_PILE] pile;
+	int32_t sauv_reg[5];
+	int32_t pile[TAILLE_PILE];
 } processus;
 
-processus[NB_PROC] TAB_PROC;
+processus TAB_PROC[NB_PROC];
 
+
+
+void init_idle(processus* tab_proc);
+
+void init_proc1(processus* tab_proc);
+
+void ctx_sw(int32_t* old_reg, int32_t* new_reg);
 
 void idle(void);
+
 void proc1(void);
 
 #endif
